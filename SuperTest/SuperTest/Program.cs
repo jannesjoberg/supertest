5de742d25d41848b593207675ba00fe7aacc7a19
Jan Sjöberg <jannesjoberg@hotmail.com>
2016-09-06 14:28:07 +0200

    Test

---
+++ SuperTest/SuperTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperTest
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Jag är en dum konsollapp.");
+
+      Console.ReadLine(); 
+    }
+  }
+}
